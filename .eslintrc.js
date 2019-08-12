d050b93f6004383388371b406f89c51ffe879ede
Rafirre <rafoaguir@gmail.com>
2019-08-12 16:20:27 -0300

    Initial commit, setup basic structure and dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "browser": true, // for predefined globals
+    "es6": true, // enables es6 features
+    "jest": true,
+  },
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "rules": {}
+};
